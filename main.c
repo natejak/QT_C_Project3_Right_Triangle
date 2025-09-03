cd67d3c92e781c619d93c22ca31b3cfe6589266e
Kajetan Zelech-Alatarvas <natejak@gmail.com>
2025-09-03 19:52:19 +0300

    Update main.c

--- main.c
+++ main.c
@@ -20,7 +20,7 @@ int main()
 // Loop ask as long as values are >0
 
     do {
-      printf("Enter base of your trinagle: ");
+      printf("Enter base of your trinagle (in meters): ");
         scanf(" %f", &USR_ENTRY_Base);   // space before %f skips the newline left in the buffer
 
         if (USR_ENTRY_Base <= 0 ) { // Must be > 0
@@ -29,7 +29,7 @@ int main()
     } while (USR_ENTRY_Base <= 0);// Repeat until valid input is provided
 
     do {
-      printf("Enter high of your trinagle: ");
+      printf("Enter high of your trinagle (in meters): ");
         scanf(" %f", &USR_ENTRY_Height);   // space before %f skips the newline left in the buffer
 
         if (USR_ENTRY_Height <= 0 ) { // Must be > 0
@@ -39,7 +39,7 @@ int main()
 
   OUT_Area = 0.5 * USR_ENTRY_Base * USR_ENTRY_Height;
 
-    printf("Provided base: %f \nProvided height: %f \nThe area of the triangle is: %f", USR_ENTRY_Base, USR_ENTRY_Height, OUT_Area);
+    printf("Provided base: %.2f m \nProvided height: %.2f m \nThe area of the triangle is: %.2f m^2", USR_ENTRY_Base, USR_ENTRY_Height, OUT_Area);
 
     return 0;
 }
